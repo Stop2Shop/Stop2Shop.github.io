e048df34476f7217de75b51ed643cfcf100693c6
suresh kumar <SunuAadhath@outlook.com>
2018-05-23 23:25:17 +0530

    Edmx and BL Layer changes Done

---
+++ Stop2ShopApp/Shop2StopBL/UserDH.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shop2StopDAL;
+
+namespace Stop2ShopBL
+{
+    public class UserDH
+    {
+        Stop2ShopContext DbContext = new Stop2ShopContext();
+        public bool LoginValidate(string username, string EncryptPwd, int RoleId)
+        {
+            var result = DbContext.Proc_UserAuthenticate(username, EncryptPwd, RoleId);
+            if (result != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Stop2ShopApp/Shop2StopCL/DatabaseUtills.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stop2ShopBL;
+
+
+namespace Stop2ShopCL
+{
+    public class DatabaseUtills
+    {
+        public bool IsAuthenticated(string UserName,String Password,int RoleId)
+        {
+            bool IsAuthenticated = false;
+            CommonUtills CUtil = new CommonUtills();
+            UserDH _UserDH = new UserDH();
+            string strEncryptedPwd = CUtil.EncryptRijndael(Password);
+            IsAuthenticated = _UserDH.LoginValidate(UserName, strEncryptedPwd, RoleId);
+            return IsAuthenticated;
+        }
+    }
+}
