f09a8a83588c9776c9604bdf7d8b1f4b5b66447e
suresh kumar <SunuAadhath@outlook.com>
2018-05-29 00:36:03 +0530

    Authentication Changes Checked

---
+++ Stop2ShopApp/Shop2StopDAL/UserDH.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stop2ShopCL;
+
+namespace Stop2ShopDAL
+{
+    public class UserDH
+    {
+        Stop2ShopContext context;
+        public UserDH()
+        {
+            context = new Stop2ShopContext();
+        }
+        public bool ValidateUser(UserDO _UserDo)
+        {
+            bool result = false;
+            try
+            {
+                var validationResult = context.Proc_UserAuthenticate(_UserDo.EmailId, _UserDo.Password, _UserDo.RoleId);
+                if (validationResult != null)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+            }
+            catch (Exception e)
+            {
+
+                result = false;
+            }
+            return result;
+        }
+
+    }
+}
--- Stop2ShopApp/Stop2ShopPL/Controllers/HomeController.cs
+++ Stop2ShopApp/Stop2ShopPL/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Stop2ShopPL.Models;
+using Stop2ShopBL;
+using Stop2ShopCL;
 
 namespace Stop2ShopPL.Controllers
 {
@@ -14,18 +16,37 @@ namespace Stop2ShopPL.Controllers
         {
             return View();
         }
+        
         [HttpPost]
         public ActionResult Login(FormCollection collection)
         {
-            Session["userType"] = collection["rbCustomerType"];
+            Session["userRole"] = collection["rbCustomerType"];
             return View();
         }
-        //[HttpPost]
-        //public ActionResult Login(UserLogin user )
-        //{
-        //    return View();
-        //}
 
+        [ActionName("LoginValidate")]
+        [HttpPost]
+        public ActionResult Login(UserLogin user)
+        {
+            UserDO _UserDO = new UserDO();
+            _UserDO.EmailId = user.EmailId;
+            _UserDO.Password = user.Password;
+            _UserDO.RoleId = user.RoleId;
+            UserManager _UserManager = new UserManager();
+            bool IsAuthenticated = _UserManager.LoginValidate(_UserDO);
+            if(IsAuthenticated)
+            {
+                //return RedirectToAction("Login", "Home");
+                ModelState.AddModelError("", "Login Authentication Success");
+                return View("Login");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Login Authentication Failed");
+                return View("Login");
+            }
+            
+        }
         // GET: Home/Details/5
         public ActionResult Details(int id)
         {
